42a9e2c8fa4eee76825da5264e5f01a5c50f1c38
786Ayush <88676009+786Ayush@users.noreply.github.com>
2023-10-03 01:11:21 +0530

    image

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -70,11 +70,11 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/hero-image.png"
+              src="/Me.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              width={700}
+              height={700}
             />
           </div>
         </motion.div>
